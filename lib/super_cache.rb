7acde40fa7bff09327475f753510e8ca41aad711
Cao Li <tsowly@hotmail.com>
2013-03-11 18:25:09 +0800

    something

--- lib/super_cache.rb
+++ lib/super_cache.rb
@@ -15,14 +15,30 @@ module SuperCache
 
   module ClassMethods
     def super_caches_page(*pages)
-      return unless perform_caching
+      return unless cache_configured?
       options = pages.extract_options!
+<<<<<<< HEAD
       filter_options = options.extract!(:if, :unless)
       filter_options[:only] = (Array(options[:only]) + pages).flatten
       cache_filter_class = options.delete(:lock) ? DogPileFilter : SimpleFilter
       around_filter filter_options do |controller, action|
         cache_filter_class.filter(options.dup, controller, action)
       end
+=======
+      filter_options = options.extract!(:if, :unless).merge(:only => (Array(options[:only]) + pages).flatten)
+      cache_options  = options.extract!(:cache_path, :expires_in).merge(:store_options => options)
+      cache_filter_class = options.delete(:lock) ? DogPileFilter : SimpleFilter
+      self.cache_filter = cache_filter_class.new cache_options
+
+      around_filter self.cache_filter, options
+    end
+
+    def skip_super_caches_page(*pages)
+      return unless self.cache_filter
+      options = pages.extract_options!
+      options[:only] = (Array(options[:only]) + pages).flatten
+      skip_around_filter self.cache_filter, options
+>>>>>>> something
     end
   end
 end
